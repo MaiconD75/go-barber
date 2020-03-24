d7a5e6461416a6a753416e921dfe1bd0daabec2d
Maicon Douglas <maicondouglas75@hotmail.com>
2020-03-23 23:06:47 -0300

    Environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '665904106dc4f6',
-    pass: '4bd6fc3816e86e',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
